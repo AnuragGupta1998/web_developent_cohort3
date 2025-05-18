d2566c5f1011725e5daddfbe42867aa548242185
Anurag Gupta <anurag6967@gmail.com>
2025-05-18 12:33:53 +0530

    comment

--- week7_mongoDB/index.js
+++ week7_mongoDB/index.js
@@ -135,7 +135,7 @@ app.post("/logout", async (req, res) => {
   });
 });
 
-//add todo of user
+//add todo routes of user
 app.post("/addTodo", authMid, async (req, res) => {
   const userID = req.user;
   console.log("add todos routes");
