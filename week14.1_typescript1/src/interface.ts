bdcceca0c5df4024ec0db475c22d9c6bd77137c9
Anurag Gupta <anurag6967@gmail.com>
2025-07-28 14:13:40 +0530

    function with object and interface

--- week14.1_typescript1/src/interface.ts
+++ week14.1_typescript1/src/interface.ts
@@ -6,10 +6,38 @@ interface User{
     email: string;
 }
 
+//interface type as function parameter with return type boolean
 function isLegalUser(user: User): boolean {
     console.log(user)
     console.log(user.age)
     return user.age >= 18;
 }
-
 console.log( isLegalUser({ name: "John", age:1 , email:""}));
+
+//interface type as function parameter without return type user interface
+function showUser(user:User):void{
+    console.log(user)
+}
+showUser({ name: "Anurag", age: 21, email: ""});
+
+
+//interface type as function parameter with return type user interface
+function userDetail(user:User):User{
+    return user;
+}
+const rst:User = userDetail({ name: "Anurag", age: 25, email: "anurag@gmail.com"});
+console.log(rst)
+console.log(userDetail({ name: "Anurag", age: 25, email: ""}));
+
+
+//user1 //interface type as function parameter with return type void
+const user1:{a:number,b:number,c:number}={
+    a:1,
+    b:2,
+    c:3
+}
+
+function sayHello(u:{a:number,b:number,c:number}):void{
+    console.log(`Hello ${u.a} ${u.b} ${u.c}`);
+}
+sayHello(user1);
