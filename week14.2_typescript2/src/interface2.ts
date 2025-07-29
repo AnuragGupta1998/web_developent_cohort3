846dde0962c6044603b06d23f5cf6c0952398a4a
Anurag Gupta <anurag6967@gmail.com>
2025-07-29 14:45:33 +0530

    interface2 file added more about interface

---
+++ week14.2_typescript2/src/interface2.ts
@@ -0,0 +1,27 @@
+interface User1{
+    name:string;
+    age:number;
+    address:{
+        street:string;
+        city:string;    
+    }
+}
+
+const user:User1 ={
+    name:"Alice",
+    age:30,
+    address:{
+        street:"123 Main St",
+        city:"Wonderland"
+    }
+}
+
+function showDetails(user:User1):User1{
+    return user;
+}
+
+const result1:User1 = showDetails(user);
+console.log("result1", result1); // Output: { name: 'Alice', age: 30, address: { street: '123 Main St', city: 'Wonderland' } }
+
+const result2 = showDetails(user);
+console.log("result2", result2); // Output: { name: 'Alice', age:
