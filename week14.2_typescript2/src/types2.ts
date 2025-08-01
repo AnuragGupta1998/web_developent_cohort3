b03b27ceb81a0f10d4a91cc0bfe5a342b9519215
Anurag Gupta <anurag6967@gmail.com>
2025-08-01 20:25:58 +0530

    unione work different in object types

--- week14.2_typescript2/src/types2.ts
+++ week14.2_typescript2/src/types2.ts
@@ -11,27 +11,30 @@ isLegalAge({ name: "John", age: 20 });
 
 
 //unions in types
-type User = {
-    name:string;
-    age:number
-}
 type Admin = {
     name:string;
     age:number;
     subject:string; 
 }
+type User = {
+    name:string;
+    age:number;
+    rollNo:Number;
+}
 
-type UserOrAdmin = Admin & User;
+// type UserOrAdmin = Admin & User;
+type UserOrAdmin = Admin | User;
 
 const user4: UserOrAdmin = {
     name: "Alice",
     age: 30,
-    subject: "Math" // This line will cause an error because 'subject' is not part of User4 
+    // subject: "Math" // This line will cause an error because 'subject' is not part of User4 
+    rollNo: 123 // Uncommenting this line will also cause an error because 'rollNo' is not part of Admin
 };  
 
 function printUserInfo(user: UserOrAdmin) {
     // console.log(`Name: ${user.name}, Age: ${user.age} Subject is `);
-    console.log(`Name: ${user.name}, Age: ${user.age} Subject is ${user.subject}`);
+    console.log(`Name: ${user.name}, Age: ${user.age} Subject is `);
     if ('subject' in user) {
         console.log(`Subject: ${user.subject}`);
     }
